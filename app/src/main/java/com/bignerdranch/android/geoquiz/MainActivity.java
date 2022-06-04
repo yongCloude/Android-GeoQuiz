dc892886b09debfe36b6d1f91bff4d56189f25e5
yongCloude <djkye0001@naver.com>
2022-06-04 22:15:55 +0900

    [feat] true 버튼과 false 버튼 객체 생성

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
@@ -3,12 +3,25 @@ package com.bignerdranch.android.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
+
+
+    public MainActivity() {
+        this.trueButton = null;
+        this.falseButton = null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
     }
 }
