aab321a8b08ad661f92c15997b3bf15b2fc8a31e
yongCloude <djkye0001@naver.com>
2022-06-05 17:15:58 +0900

    [feat] 문제 넘기는 기능 추가

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
@@ -4,17 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button trueButton;
     private Button falseButton;
+    private Button nextButton;
 
+    private TextView questionTextView;
+
+    private List<Question> questionBank;
+    private int currentIndex;
 
     public MainActivity() {
+
+        this.questionTextView = null;
         this.trueButton = null;
         this.falseButton = null;
+        this.nextButton = null;
+        this.currentIndex = 0;
+
+        questionBank = new ArrayList<>();
+        questionBank.add(new Question(R.string.question_australia, true));
+        questionBank.add(new Question(R.string.question_oceans, true));
+        questionBank.add(new Question(R.string.question_mideast, false));
+        questionBank.add(new Question(R.string.question_africa, false));
+        questionBank.add(new Question(R.string.question_americas, true));
+        questionBank.add(new Question(R.string.question_asia, true));
     }
 
     @Override
@@ -22,8 +44,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        questionTextView = findViewById(R.id.question_text_view);
+
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
+        nextButton = findViewById(R.id.next_button);
 
         trueButton.setOnClickListener(view -> {
             Toast.makeText(getApplicationContext(), R.string.correct_toast, Toast.LENGTH_SHORT)
@@ -35,5 +61,17 @@ public class MainActivity extends AppCompatActivity {
                     .show();
         });
 
+        nextButton.setOnClickListener(view -> {
+            currentIndex = (currentIndex + 1) % questionBank.size();
+            updateQuestion();
+        });
+
+        updateQuestion();
+
+    }
+
+    private void updateQuestion() {
+        int questionTextResId = questionBank.get(currentIndex).getTestResId();
+        questionTextView.setText(questionTextResId);
     }
 }
