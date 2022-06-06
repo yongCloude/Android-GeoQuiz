f2c6cfaea476270774942b8361bdd989dae8cb11
yongCloude <djkye0001@naver.com>
2022-06-06 17:42:50 +0900

    [feat] ViewModel 적용 및 Lazy Initialization 패턴 적용

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.java
@@ -1,19 +1,82 @@
 package com.bignerdranch.android.geoquiz;
 
+
 import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizViewModel extends ViewModel {
     public static final String TAG = "QuizViewModel";
 
-    public QuizViewModel() {
+    private List<Question> questionBank;
+    private int currentIndex;
+    private int answeredNum;
+    private int correctNum;
+
+
+    private QuizViewModel() {
         Log.d(TAG, "ViewModel instance created!");
+
+        this.currentIndex = 0;
+        this.answeredNum = 0;
+        this.correctNum = 0;
+
+        questionBank = new ArrayList<>();
+        questionBank.add(new Question(R.string.question_australia, true));
+        questionBank.add(new Question(R.string.question_oceans, true));
+        questionBank.add(new Question(R.string.question_mideast, false));
+        questionBank.add(new Question(R.string.question_africa, false));
+        questionBank.add(new Question(R.string.question_americas, true));
+        questionBank.add(new Question(R.string.question_asia, true));
     }
+
+    private static class LazyHolder {
+        public static final QuizViewModel INSTANCE = new QuizViewModel();
+    }
+
+    public static QuizViewModel getInstance(){
+        return LazyHolder.INSTANCE;
+    }
+
 
     @Override
     protected void onCleared() {
         super.onCleared();
         Log.d(TAG, "ViewModel instance about to be destroyed");
+    }
+
+    public Question getCurrentQuestion() {
+        return questionBank.get(currentIndex);
+    }
+
+    public int getQuestionBankSize(){
+        return questionBank.size();
+    }
+
+    public int getCurrentIndex() {
+        return currentIndex;
+    }
+
+    public void moveCurrentIndex(int direction) {
+        this.currentIndex = (this.currentIndex + direction) % questionBank.size();
+    }
+
+    public int getAnsweredNum() {
+        return answeredNum;
+    }
+
+    public void setAnsweredNum(int answeredNum) {
+        this.answeredNum = answeredNum;
+    }
+
+    public int getCorrectNum() {
+        return correctNum;
+    }
+
+    public void setCorrectNum(int correctNum) {
+        this.correctNum = correctNum;
     }
 }
