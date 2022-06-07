8a9a5c46a5438b04453b2c88867cb0ed855749a7
yongCloude <djkye0001@naver.com>
2022-06-07 18:58:21 +0900

    [feat] Intent 생성 static 함수 추가

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -2,6 +2,8 @@ package com.bignerdranch.android.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CheatActivity extends AppCompatActivity {
@@ -12,5 +14,10 @@ public class CheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+    }
+
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        return new Intent(packageContext, CheatActivity.class).putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
     }
 }
