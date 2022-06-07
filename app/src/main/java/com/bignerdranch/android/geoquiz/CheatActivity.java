2c1b2fa078cacc1b1c05a12131051fc280d4c5bb
yongCloude <djkye0001@naver.com>
2022-06-07 19:29:33 +0900

    [feat] 버튼 추가 및 전달 받은 인텐트 활용하기

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -5,15 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
     public static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
 
+    private boolean answerIsTrue = false;
+
+    private TextView answerTextView;
+    private Button showAnswerButton;
+
+    public CheatActivity() {
+        answerTextView = null;
+        showAnswerButton = null;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        answerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        answerTextView = findViewById(R.id.answer_text_view);
+        showAnswerButton = findViewById(R.id.show_answer_button);
+
+        showAnswerButton.setOnClickListener(view -> {
+            int answerText;
+            if(answerIsTrue == true){
+                answerText = R.string.true_button;
+            } else{
+                answerText = R.string.false_button;
+            }
+
+            answerTextView.setText(answerText);
+        });
     }
 
 
